817ce824b4ec082b1cd794ac9c28005a8aafef0d
Piotr Lewandowski <lewy007@o2.pl>
2023-01-23 21:51:09 +0100

    added FoodFinder with simple recipe jajecznica

---
+++ src/main/java/org/example/FoodFinder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class FoodFinder {
+    Recipe createRecipe(List<String> ingredients) {
+
+        // ingredients - cos tam zrobić z nimi
+        Recipe recipe = new Recipe("jajecznica");
+        return recipe;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        FoodFinder foodFinder = new FoodFinder();
+        List<String> ingredients = List.of("jajka", "sól", "pieprz");
+        foodFinder.createRecipe(ingredients);
         System.out.println("Hello world!");
     }
 }
